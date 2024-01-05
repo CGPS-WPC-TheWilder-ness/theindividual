651ef0e28d35e2463ae16f457090130f34c6027e
Blair <blairmunroakusa@wp.computer>
2024-01-04 17:54:51 -0900

    run.py: block out Flask dev app.run

--- run.py
+++ run.py
@@ -4,27 +4,27 @@ from server.backend import Backend_Api
 
 from json import load
 
-#if __name__ == '__main__':
-if True:
-    config = load(open('config.json', 'r'))
-    site_config = config['site_config']
-    
-    site = Website(app)
-    for route in site.routes:
-        app.add_url_rule(
-            route,
-            view_func = site.routes[route]['function'],
-            methods   = site.routes[route]['methods'],
-        )
 
-    backend_api  = Backend_Api(app, config)
-    for route in backend_api.routes:
-        app.add_url_rule(
-            route,
-            view_func = backend_api.routes[route]['function'],
-            methods   = backend_api.routes[route]['methods'],
-        )
+config = load(open('config.json', 'r'))
+site_config = config['site_config']
 
-    #print(f"Running on port {site_config['port']}")
-    #app.run(**site_config)
-    #print(f"Closing port {site_config['port']}")
+site = Website(app)
+for route in site.routes:
+    app.add_url_rule(
+        route,
+        view_func = site.routes[route]['function'],
+        methods   = site.routes[route]['methods'],
+    )
+
+backend_api  = Backend_Api(app, config)
+for route in backend_api.routes:
+    app.add_url_rule(
+        route,
+        view_func = backend_api.routes[route]['function'],
+        methods   = backend_api.routes[route]['methods'],
+    )
+if __name__ == '__main__':
+
+    print(f"Running on port {site_config['port']}")
+    app.run(**site_config)
+    print(f"Closing port {site_config['port']}")
