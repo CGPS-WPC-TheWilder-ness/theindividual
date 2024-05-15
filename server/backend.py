d8eea55c2aaaa0d233d08b46a3b2f5d0d818419f
Blair <blairmunroakusa@wp.computer>
2024-05-14 17:28:20 -0800

    update model to gpt4o

--- server/backend.py
+++ server/backend.py
@@ -119,7 +119,7 @@ class Backend_Api:
             chat_gpt_callback = ChatOpenAI(
                 temperature=0,
                 #model_name="gpt-4",
-                model_name="gpt-4-turbo-2024-04-09",
+                model_name="gpt-4o",
                 streaming=True,
                 callbacks=[callback_fn]
             )
